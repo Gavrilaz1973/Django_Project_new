4cf00e1e6c446d4140f05792f7178959575107da
Dmitry Gavrilenko <gavrilaz0076@gmail.com>
2023-08-16 16:42:06 +0300

    hw 21_1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from catalog.views import ContactView, IndexListView, ProductDetailView, BlogListView, BlogCreateView, BlogDetailView
+from catalog.views import ContactView, IndexListView, ProductDetailView, BlogListView, BlogCreateView, BlogDetailView, \
+    BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', IndexListView.as_view(), name='index'),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('<int:pk>/product/', ProductDetailView.as_view(), name='product'),
     path('blog/', BlogListView.as_view(), name='blog'),
     path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('<int:pk>/blog/', BlogDetailView.as_view(), name='read_blog')
+    path('blog/view/<int:pk>/', BlogDetailView.as_view(), name='read_blog'),
+    path('blog/edit/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
+    path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from catalog.models import Product, Blog
 
@@ -35,20 +36,52 @@ class IndexListView(ListView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_published = True)
+        return queryset
+
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'body')
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('blog')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
 
 
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
     def get_context_data(self, *args, **kwargs):
         context_data = super().get_context_data(**kwargs)
         context_data['blog_list'] = Blog.objects.get(pk=self.kwargs.get('pk'))
         return context_data
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'body')
+    # success_url = reverse_lazy('blog')
+
+    def get_success_url(self):
+        return reverse('read_blog', args=[self.kwargs.get('pk')])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog')
 
 
 
