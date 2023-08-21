4535c6ef066784776ca5c5125256571694e582ad
Dmitry Gavrilenko <gavrilaz0076@gmail.com>
2023-08-21 22:41:33 +0300

    hw 22_1 задание 1

--- catalog/forms.py
+++ catalog/forms.py
@@ -9,3 +9,24 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+    def clean_name(self):
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data = self.cleaned_data['name']
+        for i in range(len(forbidden_words)):
+            if cleaned_data.lower() == forbidden_words[i]:
+                raise forms.ValidationError('запрещенное слово')
+        return cleaned_data
+
+    def clean_description(self):
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        cleaned_data = self.cleaned_data['description']
+        for i in range(len(forbidden_words)):
+            if forbidden_words[i] in cleaned_data.lower():
+                raise forms.ValidationError('запрещенное слово')
+        return cleaned_data
+
+
+
+
+
